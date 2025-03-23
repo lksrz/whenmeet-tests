676351c93fb32b8873451421fa1ef60d9a7ba8de
Łukasz <72931508+lksrz@users.noreply.github.com>
2025-03-23 10:30:09 +0100

    Add multiple test files

---
+++ README.md
@@ -0,0 +1,9 @@
+# WhenMeet Tests
+
+Test repository for WhenMeet.me calendar scheduling application.
+
+## Running Tests
+
+```
+npm test
+```
---
+++ tests/unit-test.js
@@ -0,0 +1,7 @@
+// Unit test for WhenMeet.me
+function testAddition() {
+  console.assert(1 + 1 === 2, 'Addition works');
+  console.log('Unit test passed!');
+}
+
+testAddition();
